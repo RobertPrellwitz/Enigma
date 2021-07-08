a175800983273f708d654e12d8743af8b9f66c1c
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-07-07 23:44:24 -0500

    minor changes

--- src/server/Server.java
+++ src/server/Server.java
@@ -11,26 +11,23 @@ public class Server {
 		Server enigma = new Server();
 		enigma.run();
 	}
-	public void run () 
-	{
+	public void run () {
 		int portNum = 5764;
-		
-		try 
-		{
-		ServerSocket servSock = new ServerSocket(portNum);
-		PrintWriter writeSock = new PrintWriter(new FileOutputStream("prog2.log"),true);
 
-		while (true)
-		{
-			Socket sock = servSock.accept();
-			ServerThread servThread = new ServerThread(sock, writeSock);
-			servThread.run();
+		PrintWriter writeSock = null;
+		try {
+			ServerSocket servSock = new ServerSocket(portNum);
+			writeSock = new PrintWriter(new FileOutputStream("prog2.log"), true);
+
+			while (true) {
+				Socket sock = servSock.accept();
+				ServerThread servThread = new ServerThread(sock, writeSock);
+				servThread.run();
+				//new ServerThread(servSock.accept().run());
+			}
+		} catch (IOException exp) {
+
+			writeSock.println("ErrorLog: " + exp);
 		}
-		}
-		
-		catch (IOException exp)
-		{
-			System.out.println("ErrorLog: "+ exp);
-		}	
 	}
 }
